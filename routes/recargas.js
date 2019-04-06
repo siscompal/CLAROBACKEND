793bbfb7f8e41dfbc147f5d2bbd8f147afe719e6
isane <discuvich.ivis@gmail.com>
2019-04-05 23:02:28 -0500

    quitar autenticacion de ruta recarga

--- routes/recargas.js
+++ routes/recargas.js
@@ -6,7 +6,7 @@ var RecargasController = require('../controllers/recargas');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
-api.post('/recargas', md_auth.ensureAuth, RecargasController.DoRecarga);
+api.post('/recargas', RecargasController.DoRecarga);
 
 
 
