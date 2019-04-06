d3f8e46eb5699f00d784bd5635b1b69d13e43765
isane <discuvich.ivis@gmail.com>
2019-04-06 00:54:48 -0500

    change balance recargas

--- routes/recargas.js
+++ routes/recargas.js
@@ -7,7 +7,7 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.post('/recargas', RecargasController.DoRecarga);
-api.post('/saldo', RecargasController.getSaldo);
+api.post('/balance', RecargasController.getSaldo);
 
 
 module.exports = api;
