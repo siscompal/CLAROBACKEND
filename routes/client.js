10c458f9df6654428f69e02ff7598ed57c40463f
isane <discuvich.ivis@gmail.com>
2019-06-02 20:34:33 -0500

    ruta createClient free

--- routes/client.js
+++ routes/client.js
@@ -8,7 +8,7 @@ var md_auth = require('../middlewares/authenticated');
 
 // para utilizar un middleware se lo paso como segundo parametro a la ruta que quiero proteger con autenticacion
 api.get('/pruebas-cliente', md_auth.ensureAuth, ClientController.pruebas);
-api.post('/register', md_auth.ensureAuth, ClientController.createClient);
+api.post('/register', ClientController.createClient);
 api.post('/login', ClientController.login);
 api.put('/update/:id', md_auth.ensureAuth, ClientController.updateClient);
 api.get('/listado/', md_auth.ensureAuth, ClientController.getClients);
